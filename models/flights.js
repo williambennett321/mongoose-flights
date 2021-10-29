f1884873e77dad26ed118556455830e81ee35538
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-29 13:22:05 -0400

    create model for Flight

---
+++ models/flights.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+
+const flightSchema = new Schema ( {
+  airline: {
+    type: String,
+    enum: ["American", "Southwest", "United"]
+  },
+
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    default: "DEN"
+  },
+
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999,
+  },
+
+  departs: {
+    type: Date,
+  }
+})
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import methodOverride from "method-override"
+import ("./config/database.js")
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as flightsRouter } from './routes/flights.js'
 
 // set up app
 const app = express()
@@ -31,8 +33,9 @@ app.use(
 )
 
 // mounted routers
+app.use(methodOverride("_method"))
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/flights', flightsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
